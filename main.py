3c13cf7cd0eb785fcd093310a93c501c1e4d99e8
jeffrey <jeffrey@gmail.com>
2023-10-29 14:01:19 +0800

    引用css目錄並套用

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime
-
+import pandas as pd
 
 # 可以任意定義名稱 >> app
 # 設定伺服器
@@ -72,6 +72,22 @@ def get_bmi(name, h, w):
         ##return f"<h2>{name}</h2> <h2>BMI為:</h2><h3>{(eval(w)/eval(h)**2)*10000}</h3>"
     except Exception as e:
         return str(e)
+
+
+@app.route("/pm25")
+def get_pm25():
+    url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=CSV"
+    message = "取得資料成功!"
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        df = pd.read_csv(url).dropna()
+        columns = df.columns.tolist()
+        values = df.values.tolist()
+        print(df)
+    except Exception as e:
+        print(e)
+        message = "取得pm2.5資料失敗，請稍後再試..."
+    return render_template("pm25.html", **locals())
 
 
 # 測試程式本地端運行 僅出現一次
